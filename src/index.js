53c5879525b343146ec5ccbb8536ae99d30efcf7
Marcio Accioli <mjaccioli@gmail.com>
2023-08-31 20:14:45 -0300

    recurso de validacao por senha implementado

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { validarSenha } = require('./intermediarios');
 const rotas = require('./rotas/rotas');
 
 const app = express();
-
 app.use(express.json());
+
+app.use(validarSenha);
+
 app.use(rotas);
 
 app.listen(3000, () => {
--- src/intermediarios.js
+++ src/intermediarios.js
@@ -1,6 +1,14 @@
 const validarSenha = (req, res, next) => {
-    const { senha } = req.query;
-    if (!senha) {
-        return res.json('Necessario validar com uma senha')
+    const { senha_banco } = req.query;
+    if (!senha_banco) {
+        return res.json('Necessario validar com uma senha');
     }
+    if (senha_banco !== 'Cubos123Bank') {
+        return res.json('A senha está incorreta');
+    }
+    next();
+}
+
+module.exports = {
+    validarSenha
 }
