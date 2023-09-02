e853b07545d3adba6ea7958ef4766f5820d034a1
Marcio Accioli <mjaccioli@gmail.com>
2023-09-02 17:55:20 -0300

    Funcao criarConta atualizada

--- src/controladores/controladores.js
+++ src/controladores/controladores.js
@@ -1,5 +1,22 @@
 const { contas } = require('../bancodedados');
 
+// Validar entrada de dados
+function validarEntradasContas(req, res, nome, cpf, data_nascimento, telefone, email, senha) {
+
+    const validaCpf = contas.find(contaCpf => contaCpf.cpf === cpf);
+    const validaEmail = contas.find(contaEmail => contaEmail.email === email);
+
+    if (!nome || !cpf || !data_nascimento || !telefone || !email || !senha) {
+        return res.json({ mensagem: 'Todos os campos são obrigatórios' });
+    }
+    if (!nome.trim() || !cpf.trim() || !data_nascimento.trim() || !telefone.trim() || !email.trim() || !senha.trim()) {
+        return res.json({ mensagem: 'Todos os campos devem ser preenchidos' });
+    }
+    if (validaCpf || validaEmail) {
+        return res.json({ mensagem: 'Já existe uma conta com o cpf ou e-mail informado!' });
+    }
+}
+
 // Listar todas as contas bancarias
 const mostrarBanco = (req, res) => {
     res.json(contas);
@@ -9,18 +26,11 @@ const mostrarBanco = (req, res) => {
 
 const criarConta = (req, res) => {
     const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
-    const validaCpf = contas.find(conta => conta === cpf);
+
     let indiceContas = contas.length;
-    //const validaEmail =
-    if (!nome || !cpf || !data_nascimento || !telefone || !email || !senha) {
-        return res.json({ mensagem: 'Todos os campos são obrigatórios' });
-    }
-    if (!nome.trim() || !cpf.trim() || !data_nascimento.trim() || !telefone.trim() || !email.trim() || !senha.trim()) {
-        return res.json({ mensagem: 'Todos os campos devem ser preenchidos' });
-    }
-    if (validaCpf) {
-        return res.send('existe');
-    }
+
+    validarEntradasContas(req, res, nome, cpf, data_nascimento, telefone, email, senha);
+
     if (indiceContas === 0) {
         contas.push(req.body);
         contas[indiceContas].conta = 0;
@@ -32,7 +42,6 @@ const criarConta = (req, res) => {
         return res.json(contas);
     }
 }
-
 
 module.exports = {
     mostrarBanco,
